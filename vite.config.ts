919a68db44d4a63634c9fd641af01fa7bede123d
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-03-11 23:18:11 +0200

    Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/wish-factory/",
   plugins: [react()],
-})
+  build: {
+    outDir: 'build' // указываем каталог для сборки
+  }
+});
